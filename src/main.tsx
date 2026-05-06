9796ef4fd59f1190cb7803c651233da059c459ba
alvin-dev <ekholmperry3@gmail.com>
2026-05-06 19:08:24 +0200

    fix css import order

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { getRouter } from "./router";
- 
-const router = getRouter();
+import { router } from "./router";
  
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
